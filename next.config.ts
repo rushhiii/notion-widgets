2bce99094356b967a0cbd31f190c67420556f8aa
Tirth Patel <rushiofficial1205@gmail.com>
2026-03-25 03:15:41 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   distDir: "build",
-  output: "export",
+  // output: "export", // Removed to enable middleware
   trailingSlash: true,
   poweredByHeader: false,
 };
