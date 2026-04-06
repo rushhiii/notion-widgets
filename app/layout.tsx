065d82b7fbccfc58337c5347c08d0a1e1b8236d2
Tirth Patel <rushiofficial1205@gmail.com>
2026-04-05 22:18:05 -0400

    Update globals.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,10 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <head>
         <link
           rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Karla:wght@400;700&display=swap"
+        />
+        <link
+          rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
         />
       </head>
