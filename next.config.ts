3f7793215ca0128ea7366814d127747fa7f85db8
Tirth Patel <rushiofficial1205@gmail.com>
2026-05-02 22:01:38 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,25 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // distDir: "build", // Removed for Vercel compatibility
   // output: "export", // Removed to enable middleware
-  // trailingSlash: true,
+  trailingSlash: false,
   poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "ALLOW-FROM https://vercel.com",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' https://vercel.com",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
